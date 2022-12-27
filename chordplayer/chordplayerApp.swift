a80c8fe693e5ea0ae42c69e9dc50972205b113ff
Li Xuanji <xuanji@gmail.com>
2022-12-27 21:16:35 +0800

    Initial Commit

---
+++ chordplayer/chordplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  chordplayerApp.swift
+//  chordplayer
+//
+//  Created by Li Xuanji on 27/12/22.
+//
+
+import SwiftUI
+
+@main
+struct chordplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
